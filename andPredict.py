ff0241ce93342fc2eb0e26f52d1c3bc14c14597b
sifatkhan6 <sifatkhann229@gmail.com>
2025-02-11 15:14:58 +0800

    dataset for XOR

--- andPredict.py
+++ andPredict.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from IPython.display import display
 
 # Create truth tables/dataset for logical operations
 AND = pd.DataFrame({'x1': (0,0,1,1), 'x2': (0,1,0,1), 'y': (0,0,0,1)})
---
+++ predictXOR.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+# Create truth tables/dataset for logical operations
+XOR = pd.DataFrame({'x1': (0,0,1,1), 'x2': (0,1,0,1), 'y': (0,1,1,0)})
